aad3ce1e42ef24a6ca874f44628eb684c8a762d2
Tosie <minnaji@outlook.com>
2022-03-16 13:13:14 -0400

    commit tree fb0114de221938cc7191eb6aa0372f29282967c1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+// distanceFromHqInBlocks
+function distanceFromHqInBlocks(block){
+    const distance =42;
+    return Math.abs(block - distance)
+}
+// distanceFromHqInFeet
+function distanceFromHqInFeet(block) {
+    const distance =42;
+    return (Math.abs(block - distance)*264)
+}
+function distanceTravelledInFeet(start,destination){
+    return (Math.abs(destination - start)*264)
+}
+function calculatesFarePrice(start, destination) {
+    const dist = Math.abs(start - destination) * 264;
+    if (dist < 400) {
+        let fare = 0 * dist;
+        return fare;
+    }  else if (dist >= 400 && dist < 2000 ) {
+        return 0.02 * (dist - 400);
+    } else if (dist > 2000 && dist <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
